c45aa28e16bc7d5a8ef2c057b26ccdea2ce98b49
Joe Hitchens <joe@sleepless.com>
2015-06-26 15:39:07 -0700

    -

---
+++ peet.js
@@ -0,0 +1,43 @@
+
+PEET = {
+}
+
+PEET.build_refute_strings = function(node) {
+	// recursively massage the node objects
+	var recurse = function(nodes) {
+		var rr = []
+		for(var k in nodes) {
+			var node = nodes[k];
+			if(node.deleted != 0) {
+				continue;		// skip
+			}
+
+			var oppose = node.oppose;
+			refutes = recurse(oppose);
+			if(refutes.length == 0) {
+				node.status = "Unrefuted"
+				rr.push(node.path)
+			}
+			else {
+				node.status = "Refuted: "+refutes.join(", ")
+			}
+
+			node.creation_info = "Created "+ts2us(node.created)+" by "+(node.creator || "Someone");
+			node.modification_info = ""
+			if(node.modified) {
+				node.modification_info = "Modified "+ts2us(node.modified)+" by "+node.modifier
+			}
+
+			// abbreviated version of content str
+			node.content_abbr = node.content; //.abbr(60);
+		}
+		return rr
+	}
+
+	var o = {};
+	o[node.path] = node;
+	recurse(o);
+	return node;
+}
+
+
